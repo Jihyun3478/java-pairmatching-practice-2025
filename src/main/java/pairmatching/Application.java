51cb73be4ba538913eaba8c4a1d3043383b3c1e7
Jihyun3478 <leejihyun3478@gmail.com>
2025-12-23 14:15:14 +0900

    feat: 기능 종류 출력 및 기능을 입력받는 기능 구현
    기능 종류 출력 및 기능을 입력받는 기능 구현
    
    - 사용자가 선택할 수 있도록 기능의 종류를 출력한다.
    - 입력값이 비어있는 경우, 예외를 발생시킨다.
    - 입력값이 숫자가 아닌 경우, 예외를 발생시킨다.

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,32 @@
 package pairmatching;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Objects;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        int startFunction = getStartFunction();
+
+    }
+
+    private static int getStartFunction() {
+        while (true) {
+            try {
+                System.out.println("기능을 선택하세요.");
+                System.out.println("1. 페어 매칭");
+                System.out.println("2. 페어 조회");
+                System.out.println("3. 페어 초기화");
+                System.out.println("Q. 종료");
+
+                String input = Console.readLine();
+                if (Objects.isNull(input) || input.isEmpty()) {
+                    throw new IllegalArgumentException("[ERROR] 입력값이 비어있습니다. 다시 입력해주세요.");
+                }
+
+                return Integer.parseInt(input);
+            } catch (Exception exception) {
+                System.out.println("[ERROR] 잘못된 형식의 입력입니다. 다시 입력해주세요.");
+            }
+        }
     }
 }
