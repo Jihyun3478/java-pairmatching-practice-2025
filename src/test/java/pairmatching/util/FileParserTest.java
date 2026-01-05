19c52e3f56264ea31b2ed2783e5591b2b114a432
Jihyun3478 <leejihyun3478@gmail.com>
2026-01-05 14:57:51 +0900

    test: 로직 변경에 따른 테스트코드 수정

--- src/test/java/pairmatching/util/FileParserTest.java
+++ src/test/java/pairmatching/util/FileParserTest.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import pairmatching.domain.model.Crew;
 
 public class FileParserTest {
     @Nested
@@ -15,8 +14,8 @@ public class FileParserTest {
         @Test
         @DisplayName("페어 매칭에 필요한 크루들의 이름을 파일 입출력을 통해 불러온다.")
         void 페어_매칭에_필요한_크루들의_이름을_파일_입출력을_통해_불러온다() {
-            List<Crew> backendCrews = FileParser.parseBackendCrews();
-            List<Crew> frontendCrews = FileParser.parseFrontendCrews();
+            List<String> backendCrews = FileParser.parseBackendCrews();
+            List<String> frontendCrews = FileParser.parseFrontendCrews();
 
             assertThat(backendCrews.size()).isEqualTo(20);
             assertThat(frontendCrews.size()).isEqualTo(15);
