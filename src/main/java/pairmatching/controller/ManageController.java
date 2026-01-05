4317129bed190da28d0a2fd6f66a78f72dd02253
Jihyun3478 <leejihyun3478@gmail.com>
2026-01-05 13:59:02 +0900

    fix: 파일 입출력 후, List<String>을 반환받도록 수정

--- src/main/java/pairmatching/controller/ManageController.java
+++ src/main/java/pairmatching/controller/ManageController.java
@@ -1,12 +1,11 @@
 package pairmatching.controller;
 
 import java.util.List;
-import pairmatching.domain.model.Crew;
 import pairmatching.util.FileParser;
 
 public class ManageController {
     public void start() {
-        List<Crew> backendCrews = FileParser.parseBackendCrews();
-        List<Crew> frontendCrews = FileParser.parseFrontendCrews();
+        List<String> backendCrews = FileParser.parseBackendCrews();
+        List<String> frontendCrews = FileParser.parseFrontendCrews();
     }
 }
--- src/main/java/pairmatching/util/FileParser.java
+++ src/main/java/pairmatching/util/FileParser.java
@@ -1,29 +1,21 @@
 package pairmatching.util;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
-import pairmatching.domain.model.Course;
-import pairmatching.domain.model.Crew;
 
 public class FileParser {
 	private static final String BACKEND_FILE_PATH = "src/main/resources/backend-crew.md";
 	private static final String FRONTEND_FILE_PATH = "src/main/resources/frontend-crew.md";
 
-	public static List<Crew> parseBackendCrews() {
-		return getCrews(BACKEND_FILE_PATH, Course.BACKEND);
+	public static List<String> parseBackendCrews() {
+		return getCrews(BACKEND_FILE_PATH);
 	}
 
-	public static List<Crew> parseFrontendCrews() {
-		return getCrews(FRONTEND_FILE_PATH, Course.FRONTEND);
+	public static List<String> parseFrontendCrews() {
+		return getCrews(FRONTEND_FILE_PATH);
 	}
 
-	private static List<Crew> getCrews(String backendFilePath, Course course) {
-		List<String> names = FileReader.readFile(new File(backendFilePath));
-		List<Crew> crews = new ArrayList<>();
-		for (String name : names) {
-			crews.add(new Crew(course, name));
-		}
-		return crews;
+	private static List<String> getCrews(String filePath) {
+		return FileReader.readFile(new File(filePath));
 	}
 }
